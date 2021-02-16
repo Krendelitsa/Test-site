7c1aac6fdccefd33b81a0b3612646a2bdcd3fc30
TitanRat <titanrat@bk.ru>
2021-02-16 20:13:39 +0300

    initial commit

---
+++ test.js
@@ -0,0 +1,7 @@
+/*
+А вот так можно сделать чтобы функция textTick выполнялась раз в 3000 милисекунд
+let timerId = setInterval(() => testTick(), 3000);
+
+function testTick(){
+    alert('Делаю что то');
+}*/
